7687525956ae9cdb95d70b0dc68dc4648d911536
yoryet <y.danoun@pharmatender.cl>
2024-04-14 15:38:56 -0400

    feat : add Venta model and create  fx getTotalVenta

---
+++ src/main/java/com/ventas/ventas/Models/Venta.java
@@ -0,0 +1,60 @@
+package com.ventas.ventas.Models;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "ventas")
+public class Venta {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany
+    @JoinTable(
+        name = "venta_productos",
+        joinColumns = @JoinColumn(name = "venta_id"),
+        inverseJoinColumns = @JoinColumn(name = "producto_id")
+    )
+    @JsonManagedReference
+    private List<Producto> productos;
+
+    @Column(name = "fecha_venta")
+    private LocalDate fechaVenta;
+
+    public Venta() {}
+
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
+    public LocalDate getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(LocalDate fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public BigDecimal getTotalVenta() {
+        return productos.stream()
+                        .map(Producto::getPrecio)
+                        .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+}
