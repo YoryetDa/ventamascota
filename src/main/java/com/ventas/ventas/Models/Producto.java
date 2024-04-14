2a35ddb924ceaec5ba807db4c74ff3346b22a685
yoryet <y.danoun@pharmatender.cl>
2024-04-14 15:38:26 -0400

    feat : add Producto Model

---
+++ src/main/java/com/ventas/ventas/Models/Producto.java
@@ -0,0 +1,61 @@
+package com.ventas.ventas.Models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.util.*;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "precio")
+    private BigDecimal precio;
+
+    @ManyToMany(mappedBy = "productos")
+    @JsonBackReference
+    private List<Venta> ventas;
+
+    public Producto() {}
+
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public List<Venta> getVentas() {
+        return ventas;
+    }
+
+    public void setVentas(List<Venta> ventas) {
+        this.ventas = ventas;
+    }
+    
+}
